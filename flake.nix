f6629aeb3a422ea0caba38adb4d70be9ebc3c2fa
Sam Mahoney <mahoney@cmui.co.uk>
2026-05-21 23:53:13 +0100

    Add nix flake with ADB (android-tools) and direnv setup

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Dev shell with ADB (android-tools)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "aarch64-darwin" "x86_64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.android-tools ];
+        };
+      });
+    };
+}
